f0e77c4fd4933c0139c0bab5dda6eb3f941d3371
Moon Beom-soo <105046055+guluming@users.noreply.github.com>
2023-03-08 00:47:20 +0900

    Feat: 회원정보 조회 기능 - 접속한 유저와 조회 하려는 유저id를 비교 - 자기 자신일 경우 true - 다른 회원일 경우 false

--- src/main/java/com/slamdunk/WORK/dto/response/UserResponse.java
+++ src/main/java/com/slamdunk/WORK/dto/response/UserResponse.java
@@ -1,4 +1,16 @@
 package com.slamdunk.WORK.dto.response;
 
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.Setter;
+
+@AllArgsConstructor
+@Getter
+@Setter
 public class UserResponse {
+    private boolean myInfo;
+    private Long userId;
+    private String email;
+    private String team;
+    private String teamposition;
 }
