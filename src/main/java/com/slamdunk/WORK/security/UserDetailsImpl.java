fcd30b819c059a162a395be78853546315f8617d
Moon Beom-soo <105046055+guluming@users.noreply.github.com>
2023-03-07 23:16:36 +0900

    Feat: Spring Security 및 JWT - 시큐리티와 jWT 기능 추가 - 단 JWT의 경우 토큰에서 추가적인 변경 필요할 수도 있음

--- src/main/java/com/slamdunk/WORK/security/UserDetailsImpl.java
+++ src/main/java/com/slamdunk/WORK/security/UserDetailsImpl.java
@@ -1,4 +1,66 @@
 package com.slamdunk.WORK.security;
 
-public class UserDetailsImpl {
+import com.slamdunk.WORK.entity.User;
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.authority.SimpleGrantedAuthority;
+import org.springframework.security.core.userdetails.UserDetails;
+
+import java.util.ArrayList;
+import java.util.Collection;
+
+public class UserDetailsImpl implements UserDetails {
+    private final User user;
+//    private Long id;
+//    private String nickname;
+//    private UserRoleEnum role;
+
+    public UserDetailsImpl(User user) {
+        this.user = user;
+    }
+
+    public User getUser() {
+        return user;
+    }
+
+    @Override
+    public Collection<? extends GrantedAuthority> getAuthorities() {
+//        MemberRoleEnum role = member.getRole();
+        String authority = "TeamMember";
+
+        SimpleGrantedAuthority simpleGrantedAuthority = new SimpleGrantedAuthority(authority);
+        Collection<GrantedAuthority> authorities = new ArrayList<>(); //List인 이유 : 여러개의 권한을 가질 수 있다
+        authorities.add(simpleGrantedAuthority);
+
+        return authorities;
+    }
+
+    @Override
+    public String getPassword() {
+        return user.getPassword();
+    }
+
+    @Override
+    public String getUsername() {
+        return user.getEmail();
+    }
+
+    @Override
+    public boolean isAccountNonExpired() {
+        return true;
+    }
+
+    @Override
+    public boolean isAccountNonLocked() {
+        return true;
+    }
+
+    @Override
+    public boolean isCredentialsNonExpired() {
+        return true;
+    }
+
+    @Override
+    public boolean isEnabled() {
+        return true;
+    }
 }
--- src/main/java/com/slamdunk/WORK/security/UserDetailsServiceImpl.java
+++ src/main/java/com/slamdunk/WORK/security/UserDetailsServiceImpl.java
@@ -1,4 +1,22 @@
 package com.slamdunk.WORK.security;
 
-public class UserDetailsServiceImpl {
+import com.slamdunk.WORK.entity.User;
+import com.slamdunk.WORK.repository.UserRepository;
+import lombok.RequiredArgsConstructor;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
+import org.springframework.stereotype.Service;
+
+@RequiredArgsConstructor
+@Service
+public class UserDetailsServiceImpl implements UserDetailsService {
+    private final UserRepository userRepository;
+
+    @Override
+    public UserDetails loadUserByUsername(String email) throws UsernameNotFoundException {
+        User user = userRepository.findByEmail(email).orElseThrow(
+                ()-> new UsernameNotFoundException("등록되지 않았거나 탈퇴한 사용자 입니다"));
+        return new UserDetailsImpl(user);
+    }
 }
