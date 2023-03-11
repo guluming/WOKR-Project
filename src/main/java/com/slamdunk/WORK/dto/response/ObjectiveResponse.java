9758c74ef46d4e80407130ad92e43b246c539953
Moon Beom-soo <105046055+guluming@users.noreply.github.com>
2023-03-12 01:04:57 +0900

    Refactor: 목표 전체, 상세 조회 - Builder를 사용

--- src/main/java/com/slamdunk/WORK/dto/response/ObjectiveResponse.java
+++ src/main/java/com/slamdunk/WORK/dto/response/ObjectiveResponse.java
@@ -1,19 +1,31 @@
 package com.slamdunk.WORK.dto.response;
 
-import lombok.AllArgsConstructor;
-import lombok.Getter;
-import lombok.Setter;
+import lombok.*;
 
 import java.time.LocalDate;
 
-@AllArgsConstructor
+@NoArgsConstructor
 @Getter
-@Setter
 public class ObjectiveResponse {
+    private boolean myObjective;
     private Long objectiveId;
     private String objective;
     private LocalDate startdate;
     private LocalDate enddate;
     private int color;
     private int progress;
+
+    @Builder
+    public ObjectiveResponse(
+            boolean myObjective, Long objectiveId, String objective,
+            LocalDate startdate, LocalDate enddate, int color,
+            int progress) {
+        this.myObjective = myObjective;
+        this.objectiveId = objectiveId;
+        this.objective = objective;
+        this.startdate = startdate;
+        this.enddate = enddate;
+        this.color = color;
+        this.progress = progress;
+    }
 }
