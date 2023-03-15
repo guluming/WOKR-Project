f734a8608b6f7a33be9ba1ea292632ade35a27e3
Moon Beom-soo <105046055+guluming@users.noreply.github.com>
2023-03-15 11:47:09 +0900

    Fix: CORS 수정 - 프론트엔드 도메인 주소 추가

--- src/main/java/com/slamdunk/WORK/security/config/WebConfig.java
+++ src/main/java/com/slamdunk/WORK/security/config/WebConfig.java
@@ -23,8 +23,9 @@ public class WebConfig implements WebMvcConfigurer {
 
                 // 주소 수정 부분
                 .allowedOrigins(
-                        "https://localhost:3000",
-                        "http://localhost:3000")
+                        "https://www.wokr.kr", "http://www.wokr.kr",
+                        "https://wokr.kr", "http://wokr.kr",
+                        "https://localhost:3000", "http://localhost:3000")
                 .allowedMethods("POST", "GET", "PATCH", "DELETE", "HEAD", "OPTIONS")
                 .allowCredentials(true);
     }
