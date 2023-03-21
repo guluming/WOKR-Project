97768b6969e796373cf2a7bae11cdd600474a12b
minjpark3 <minj9850610@gmail.com>
2023-03-21 11:11:55 +0900

    Fix: 할일 삭제 API, - 할일 삭제  API 로직 수정 - ToDoResponse 어노테이션 수정

--- src/main/java/com/slamdunk/WORK/dto/response/ToDoResponse.java
+++ src/main/java/com/slamdunk/WORK/dto/response/ToDoResponse.java
@@ -11,13 +11,11 @@ import java.time.LocalDateTime;
 
 
 @Getter
-@Builder
-@AllArgsConstructor
 @NoArgsConstructor
 public class ToDoResponse {
     private boolean myToDo;
-    private Long toDoId;
     private Long keyResultId;
+    private Long toDoId;
     private String toDo;
     private String memo;
     private LocalDate startDate;
@@ -30,19 +28,24 @@ public class ToDoResponse {
     private boolean completion;
     private String color;
 
-//    public ToDoResponse(boolean myToDo,Long toDoId,
-//                        String toDo, String memo, LocalDateTime startDate,
-//                        LocalDateTime endDate,int priority, boolean display ){
-//        this.myToDo = myToDo;
-//        this.toDoId = toDoId;
-//        this.toDo = toDo;
-//        this.memo =memo;
-//        this.startDate = startDate;
-//        this.endDate = endDate;
-//        this.priority =priority;
-//        this.display = display;
-//    }
-
-
+    @Builder
+    public ToDoResponse(boolean myToDo, Long keyResultId, Long toDoId, String toDo, String memo,
+                              LocalDate startDate, String startDateTime, LocalDate endDate, String endDateTime,
+                              String fstartDate, String fendDate, int priority, boolean completion, String color ) {
+        this.myToDo = myToDo;
+        this.keyResultId = keyResultId;
+        this.toDoId = toDoId;
+        this.toDo = toDo;
+        this.memo = memo;
+        this.startDate = startDate;
+        this.startDateTime = startDateTime;
+        this.endDate = endDate;
+        this.endDateTime = endDateTime;
+        this.fstartDate = fstartDate;
+        this.fendDate = fendDate;
+        this.priority = priority;
+        this.completion = completion;
+        this.color = color;
+    }
 
 }
