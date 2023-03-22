501c14db1deb7ef5580d23dff9f9c8dc0b8bf043
Moon Beom-soo <105046055+guluming@users.noreply.github.com>
2023-03-23 01:37:48 +0900

    Feat: ToDo 날짜별 전체 조회 - 응답을 위한 ResponseDTO 생성

---
+++ src/main/java/com/slamdunk/WORK/dto/response/ToDoProgressResponse.java
@@ -0,0 +1,100 @@
+package com.slamdunk.WORK.dto.response;
+
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+import java.time.LocalDate;
+import java.util.List;
+
+@NoArgsConstructor
+@Getter
+public class ToDoProgressResponse {
+    private String targetDate;
+    private List<progressTodo> progressTodo;
+    private List<completionTodo> completionTodo;
+    @NoArgsConstructor
+    @Getter
+    public static class progressTodo{
+        private boolean myToDo;
+        private Long keyResultId;
+        private Long toDoId;
+        private String toDo;
+        private String memo;
+        private LocalDate startDate;
+        private String startDateTime;
+        private LocalDate endDate;
+        private String endDateTime;
+        private String fstartDate;
+        private String fendDate;
+        private int priority;
+        private boolean completion;
+        private String color;
+
+        @Builder
+        public progressTodo(boolean myToDo, Long keyResultId, Long toDoId, String toDo, String memo,
+                            LocalDate startDate, String startDateTime, LocalDate endDate, String endDateTime,
+                            String fstartDate, String fendDate, int priority, boolean completion, String color ) {
+            this.myToDo = myToDo;
+            this.keyResultId = keyResultId;
+            this.toDoId = toDoId;
+            this.toDo = toDo;
+            this.memo = memo;
+            this.startDate = startDate;
+            this.startDateTime = startDateTime;
+            this.endDate = endDate;
+            this.endDateTime = endDateTime;
+            this.fstartDate = fstartDate;
+            this.fendDate = fendDate;
+            this.priority = priority;
+            this.completion = completion;
+            this.color = color;
+        }
+    }
+
+    @NoArgsConstructor
+    @Getter
+    public static class completionTodo{
+        private boolean myToDo;
+        private Long keyResultId;
+        private Long toDoId;
+        private String toDo;
+        private String memo;
+        private LocalDate startDate;
+        private String startDateTime;
+        private LocalDate endDate;
+        private String endDateTime;
+        private String fstartDate;
+        private String fendDate;
+        private int priority;
+        private boolean completion;
+        private String color;
+
+        @Builder
+        public completionTodo(boolean myToDo, Long keyResultId, Long toDoId, String toDo, String memo,
+                            LocalDate startDate, String startDateTime, LocalDate endDate, String endDateTime,
+                            String fstartDate, String fendDate, int priority, boolean completion, String color ) {
+            this.myToDo = myToDo;
+            this.keyResultId = keyResultId;
+            this.toDoId = toDoId;
+            this.toDo = toDo;
+            this.memo = memo;
+            this.startDate = startDate;
+            this.startDateTime = startDateTime;
+            this.endDate = endDate;
+            this.endDateTime = endDateTime;
+            this.fstartDate = fstartDate;
+            this.fendDate = fendDate;
+            this.priority = priority;
+            this.completion = completion;
+            this.color = color;
+        }
+    }
+
+    @Builder
+    public ToDoProgressResponse(String targetDate, List<progressTodo> progressTodo, List<completionTodo> completionTodo) {
+        this.targetDate = targetDate;
+        this.progressTodo = progressTodo;
+        this.completionTodo = completionTodo;
+    }
+}
