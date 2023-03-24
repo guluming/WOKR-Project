61e20c896cc1f6126ae5047785326b22ed8da562
Moon Beom-soo <105046055+guluming@users.noreply.github.com>
2023-03-24 20:19:54 +0900

    fix: ToDo 날짜별 전체 조회 - 삭제된 ToDO는 조회되지 않도록 쿼리 수정

--- src/main/java/com/slamdunk/WORK/repository/UserToDoRepository.java
+++ src/main/java/com/slamdunk/WORK/repository/UserToDoRepository.java
@@ -19,17 +19,17 @@ public interface UserToDoRepository extends JpaRepository<UserToDo, Long> {
         List<UserToDo> findAllByObjectiveIdAndUserId(@Param("objectiveId") Long objectiveId, @Param("userId") Long userId);
         @Query("select ut from UserToDo ut inner join KeyResult kr on ut.keyResult.id = kr.id where ut.keyResult.id = :keyResultId AND ut.user.id = :userId AND kr.deleteState = false")
         List<UserToDo> findAllByKeyResultIdAndUserId(@Param("keyResultId") Long keyResultId, @Param("userId") Long userId);
-        @Query("select ut from UserToDo ut inner join ToDo t on ut.toDo.id = t.id where ut.user.id = :userId AND t.completion = false AND t.endDate >= :today order by t.endDate")
+        @Query("select ut from UserToDo ut inner join ToDo t on ut.toDo.id = t.id where ut.user.id = :userId AND t.deleteState = false AND t.completion = false AND t.endDate >= :today order by t.endDate")
         List<UserToDo> findAllByUserIdAndCompletionFalseAndProgress(@Param("userId") Long userId, @Param("today")LocalDate today);
-        @Query("select ut from UserToDo ut inner join ToDo t on ut.toDo.id = t.id where ut.team = :team AND t.completion = false AND t.endDate >= :today order by t.endDate")
+        @Query("select ut from UserToDo ut inner join ToDo t on ut.toDo.id = t.id where ut.team = :team AND t.deleteState = false AND t.completion = false AND t.endDate >= :today order by t.endDate")
         List<UserToDo> findAllByTeamAndCompletionFalseAndProgress(@Param("team") String team, @Param("today")LocalDate today);
         @Query("select ut from UserToDo ut inner join ToDo t on ut.toDo.id = t.id where ut.user.id = :userId AND t.completion = true order by t.endDate")
         List<UserToDo> findAllByUserIdAndCompletionTrueAndCompletion(@Param("userId") Long userId);
-        @Query("select ut from UserToDo ut inner join ToDo t on ut.toDo.id = t.id where ut.team = :team AND t.completion = true order by t.endDate")
+        @Query("select ut from UserToDo ut inner join ToDo t on ut.toDo.id = t.id where ut.team = :team AND t.deleteState = false AND t.completion = true order by t.endDate")
         List<UserToDo> findAllByTeamAndCompletionTrueAndCompletion(@Param("team") String team);
-        @Query("select ut from UserToDo ut inner join ToDo t on ut.toDo.id = t.id where ut.user.id = :userId AND t.endDate >= :today order by t.endDate desc")
+        @Query("select ut from UserToDo ut inner join ToDo t on ut.toDo.id = t.id where ut.user.id = :userId AND t.deleteState = false AND t.endDate >= :today order by t.endDate desc")
         List<UserToDo> findByUserIdAndLastEndDate(@Param("userId") Long userId, @Param("today")LocalDate today);
-        @Query("select ut from UserToDo ut inner join ToDo t on ut.toDo.id = t.id where ut.user.id = :userId AND t.completion = true AND t.endDate < :today order by t.endDate")
+        @Query("select ut from UserToDo ut inner join ToDo t on ut.toDo.id = t.id where ut.user.id = :userId AND t.deleteState = false AND t.completion = true AND t.endDate < :today order by t.endDate")
         List<UserToDo> findByUserIdAndFirstEndDate(@Param("userId") Long userId, @Param("today")LocalDate today);
 
         List<UserToDo> findAllByUserIdIn(List<Long> teamMemberIds);
