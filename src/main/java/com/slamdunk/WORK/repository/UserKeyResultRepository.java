faf8e26f7d4d941c53b78708da7b00c3db5b8c37
Moon Beom-soo <105046055+guluming@users.noreply.github.com>
2023-03-24 19:29:44 +0900

    작업 임시 저장

--- src/main/java/com/slamdunk/WORK/repository/UserKeyResultRepository.java
+++ src/main/java/com/slamdunk/WORK/repository/UserKeyResultRepository.java
@@ -2,6 +2,7 @@ package com.slamdunk.WORK.repository;
 
 import com.slamdunk.WORK.entity.UserKeyResult;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 
 import java.util.List;
 import java.util.Optional;
@@ -9,5 +10,7 @@ import java.util.Optional;
 public interface UserKeyResultRepository extends JpaRepository<UserKeyResult, Long> {
     List<UserKeyResult> findAllByTeam(String team);
     Optional<UserKeyResult> findByKeyResultIdAndUserId(Long KeyResultId, Long userId);
+//    List<UserKeyResult> findAllByObjectiveIdAndUserId(Long objectiveId, Long userId);
+    @Query("select ukr from UserKeyResult ukr inner join KeyResult kr on ukr.keyResult.id = kr.id where ukr.objective.id = :objectiveId AND ukr.user.id = :userId AND ")
     List<UserKeyResult> findAllByObjectiveIdAndUserId(Long objectiveId, Long userId);
 }
