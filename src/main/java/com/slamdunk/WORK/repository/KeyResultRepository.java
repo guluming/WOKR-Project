597bdd5e62721767b3755d138cdfe30401406ae3
Moon Beom-soo <105046055+guluming@users.noreply.github.com>
2023-03-28 03:52:41 +0900

    Fix: 핵심결과 진척도 수정, 핵심결과 자신감 수정, 핵심결과 수정 api - 같은 팀이고 권한이 팀장이면 모두 수정 가능하도록 변경

--- src/main/java/com/slamdunk/WORK/repository/KeyResultRepository.java
+++ src/main/java/com/slamdunk/WORK/repository/KeyResultRepository.java
@@ -9,6 +9,8 @@ import java.util.List;
 import java.util.Optional;
 
 public interface KeyResultRepository extends JpaRepository<KeyResult, Long> {
+    @Query("SELECT k FROM KeyResult k inner join UserKeyResult ukr on k.id = ukr.keyResult.id WHERE k.id = :keyResultId AND ukr.team = :team AND k.deleteState = false")
+    Optional<KeyResult> findByKeyResultIdAndTeam(@Param("keyResultId")Long keyResultId, @Param("team")String team);
     @Query("SELECT k FROM KeyResult k WHERE k.objective.id = :ObjectiveId AND k.deleteState = false")
     List<KeyResult> findAllByObjectiveId(@Param("ObjectiveId") Long ObjectiveId);
     @Query("SELECT k FROM KeyResult k WHERE k.id = :keyResultId AND k.deleteState = false")
